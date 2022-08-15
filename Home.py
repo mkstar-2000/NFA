875c8db1be8a921f1806eb40f01721b503e945f3
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2022-08-14 21:15:38 -0400

    Deploy from here

---
+++ Home.py
@@ -0,0 +1,32 @@
+# web development - Dashboard
+import streamlit as st 
+
+
+st.set_page_config(
+    page_title="About",
+    page_icon="🇺🇸",
+    layout= "wide"    
+)
+
+st.write("# Welcome to NotFinancialAdvice! 👋")
+
+st.markdown(
+    """
+    This is your one stop shop for all analysis as it relates to the Stock Markets. 
+    Whether you are intersted in general information on the company, fundamental analysis, technical analysis 
+    or perhaps you want to see what your asset looks like forecasted in the future, we have the tools you are
+    looking for!
+ 
+    
+    **👈 Select a page from the sidebar** to see a variety of financial analysis!
+    
+    ### Feel Free to connect!
+    - [Richie Garafola](https://www.linkedin.com/in/richie-garafola)
+    - [Jacob Edelbrock](https://www.linkedin.com/in/jacob-edelbrock-resume/)
+    - [Mark Staten](https://www.linkedin.com/in/mark-staten-b9b3885/)
+        
+"""
+)
+
+
+
