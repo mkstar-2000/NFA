a3ee61ce1ef2a502c51712168299db2bf45f8797
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2022-08-15 22:17:31 -0400

    current landing page

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ st.set_page_config(
 )
 
 st.write("# Welcome to NotFinancialAdvice! 👋")
-
+st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/LandingPage.jpg")
 st.markdown(
     """
     This is your one stop shop for all analysis as it relates to the Stock Markets. 
@@ -28,5 +28,51 @@ st.markdown(
 """
 )
 
+
+tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9 = st.tabs(["Agenda", "Executive Summary", "High Level Architechtural Diagram", "NFT Token Gating: Proof of Concept", "Financial Analysis Dashboard", "Project Approach", "Oracle Database", "Next Steps", "Results and Conclusions"])
+
+with tab1:
+    st.header("Agenda")
+    st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/agenda.jpg")
+
+with tab2:
+    st.header("Executive Summary")
+    st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/executive_summary.jpg")
+
+with tab3:
+    st.header("High Level Architechtural Diagram")
+    st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/high_level_diagram.png")
+    
+with tab4:
+    st.header("NFT Token Gating: Proof of Concept")
+    st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/nft_token_gating.png")
+    st.markdown("""
+    [Part 1 NFA ERC 1155 Contract and Mint](https://www.youtube.com/watch?v=jiKPlbJcd3Q)
+    
+    [Part 2 NFA ERC1155 Etherscan Mint Transaction](https://www.youtube.com/watch?v=3KPBPvwWlYA)
+    
+    [Part 3 NFA ERC1155 Safe Transfer NFT](https://www.youtube.com/watch?v=Z6vmJKkdzRo)
+    
+    [Part 4 NFA ERC 1155 Etherscan NFT Transfer Details](https://www.youtube.com/watch?v=NP3PUB-6gyQ)""")
+
+with tab5:
+    st.header("Financial Analysis Dashboard")
+    st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/dashboardoverview.png")
+    
+with tab6:
+    st.header("Oracle Database")
+    st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/oracle_database.png")
+    
+with tab7:
+    st.header("Project Approach")
+    st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/project_approach.png")    
+    
+with tab8:
+    st.header("Next Steps")
+    st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/next_steps.png")    
+
+with tab9:
+    st.header("Results and Conclusions")
+    st.image("Desktop/Github/Project3/Project3/Richie/Streamlit/Resources/results.jpg")
 
 
